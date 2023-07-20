3b1f71a418548543fbf19f6f8a4db229eaaeecd9
Michael Stevens <michael.stevens@hpponline.co.uk>
2023-07-20 12:32:08 +0100

    Testing

---
+++ Testing2.py
@@ -0,0 +1,72 @@
+import time
+import cv2 as cv
+import numpy as np
+import blobconverter
+import depthai as dai
+
+
+def frame_norm(frame, bbox):
+    norm_vals = np.full(len(bbox), frame.shape[0])
+    norm_vals[::2] = frame.shape[1]
+    return (np.clip(np.array(bbox), 0, 1) * norm_vals).astype(int)
+
+
+pipeline = dai.Pipeline()
+
+cam_rgb = pipeline.create(dai.node.ColorCamera)
+cam_rgb.setPreviewSize(300, 300)
+cam_rgb.setInterleaved(False)
+
+detection_nn = pipeline.create(dai.node.MobileNetDetectionNetwork)
+detection_nn.setBlobPath(blobconverter.from_zoo(name='mobilenet-ssd', shaves=6))
+detection_nn.setConfidenceThreshold(0.5)
+
+cam_rgb.preview.link(detection_nn.input)
+
+xout_rgb = pipeline.create(dai.node.XLinkOut)
+xout_rgb.setStreamName('rgb')
+cam_rgb.preview.link(xout_rgb.input)
+
+xout_nn = pipeline.create(dai.node.XLinkOut)
+xout_nn.setStreamName('nn')
+detection_nn.out.link(xout_nn.input)
+
+label_map = ['background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow',
+             'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
+
+with dai.Device(pipeline) as device:
+    q_rgb = device.getOutputQueue('rgb')
+    q_nn = device.getOutputQueue('nn')
+    frame = None
+    detections = []
+
+    fc = 0
+    fps = 0
+    start = time.time()
+    while 1:
+        fc += 1
+        if time.time() - start > 1:
+            fps = fc
+            fc = 0
+            start = time.time()
+
+        in_rgb = q_rgb.tryGet()
+        in_nn = q_nn.tryGet()
+
+        if in_rgb is not None:
+            frame = in_rgb.getCvFrame()
+
+        if in_nn is not None:
+            detections = in_nn.detections
+
+        if frame is not None:
+            for detection in detections:
+                bbox = frame_norm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
+                cv.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
+                cv.putText(frame, label_map[detection.label], (bbox[0], bbox[1]), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv.LINE_AA)
+                cv.putText(frame, f'{fps} FPS', (10, 10), cv.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), 1, cv.LINE_AA)
+
+            cv.imshow('preview', frame)
+
+        if cv.waitKey(1) == ord('q'):
+            break
