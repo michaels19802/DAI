82ca0ebb819f8ccbf15d421d3f3babf65fb61b9b
Michael Stevens <michael.stevens@hpponline.co.uk>
2023-07-26 12:03:59 +0100

    Testing

--- Testing7.py
+++ Testing7.py
@@ -1,5 +1,5 @@
 import depthai as dai
-from depthai_sdk import OakCamera
+from depthai_sdk import OakCamera, TextPosition, BboxStyle
 
 
 with OakCamera() as oak:
@@ -7,5 +7,12 @@ with OakCamera() as oak:
     nn = oak.create_nn('yolov6nr3_coco_640x352', color, spatial=True)
     nn.config_spatial(bb_scale_factor=.5, lower_threshold=300, upper_threshold=10000,
                       calc_algo=dai.SpatialLocationCalculatorAlgorithm.AVERAGE)
-    oak.visualize([nn.out.main], fps=True)
+    visualizer = oak.visualize(nn.out.main, fps=True)
+
+    visualizer.detections(bbox_style=BboxStyle.RECTANGLE,
+                          label_position=TextPosition.TOP_LEFT,
+                          fill_transparency=.5,
+                          thickness=2
+                          ).text(auto_scale=False, font_thickness=1, outline_color=(255, 255, 255))
+
     oak.start(blocking=True)
