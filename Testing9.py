520fd37eba9a4fb6c16ebd9d35750b007d283aee
Michael Stevens <michaels1980@gmail.com>
2023-07-28 15:08:14 +0100

    Testing

--- Testing9.py
+++ Testing9.py
@@ -3,5 +3,5 @@ from depthai_sdk import OakCamera
 
 with OakCamera() as oak:
     cams = oak.create_all_cameras()
-    oak.visualize(cams, scale=.3)
+    oak.visualize(cams, scale=.7, fps=True)
     oak.start(blocking=True)
