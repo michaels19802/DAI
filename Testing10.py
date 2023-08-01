e982eb163803cf5f38e9b97e31eb371091bd6f7d
Michael Stevens <michaels1980@gmail.com>
2023-08-01 10:44:13 +0100

    Testing

--- Testing10.py
+++ Testing10.py
@@ -1,9 +1,7 @@
 from depthai_sdk import OakCamera
 
 
-with OakCamera(rotation=90) as oak:
+with OakCamera() as oak:
     color = oak.create_camera('color', resolution='1080p')
-    left = oak.create_camera('left', resolution='400p')
-    right = oak.create_camera('right', resolution='400p')
-    oak.visualize([color, left, right], fps=True)
+    oak.visualize([color], fps=True)
     oak.start(blocking=True)
