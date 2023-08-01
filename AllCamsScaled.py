824612cb0e658fb1a993e61b0da6a8da646c2e62
Michael Stevens <michaels1980@gmail.com>
2023-08-01 16:19:54 +0100

    Testing

--- AllCamsScaled.py
+++ AllCamsScaled.py
@@ -3,5 +3,5 @@ from depthai_sdk import OakCamera
 
 with OakCamera() as oak:
     cams = oak.create_all_cameras()
-    oak.visualize(cams, scale=.7, fps=True)
+    oak.visualize(cams, scale=.4, fps=True)
     oak.start(blocking=True)
--- FaceDetection.py
+++ FaceDetection.py
@@ -2,7 +2,6 @@ from depthai_sdk import OakCamera
 
 with OakCamera() as oak:
     color = oak.create_camera('color')
-    # nn = oak.create_nn('mobilenet-ssd', color)
     nn = oak.create_nn('face-detection-retail-0004', color)
     oak.visualize([nn], fps=True)
     oak.start(blocking=True)
