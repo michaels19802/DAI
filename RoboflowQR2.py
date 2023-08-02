d6b12bf28191ae3d024240bc7163cae500f392cf
Michael Stevens <michael.stevens@hpponline.co.uk>
2023-08-02 11:53:09 +0100

    Testing

--- RoboflowQR2.py
+++ RoboflowQR2.py
@@ -1,8 +1,13 @@
-from depthai_sdk import OakCamera, TextPosition, BboxStyle
+import depthai as dai
+from depthai_sdk import OakCamera, TextPosition
 
 
 with OakCamera() as oak:
     color = oak.create_camera('color')
+
+    color.config_color_camera(scene_mode=dai.CameraControl.SceneMode.BARCODE,
+                              awb_mode=dai.CameraControl.AutoWhiteBalanceMode.DAYLIGHT)
+
     model_config = {
         'source': 'roboflow',
         'model': 'barcode-and-qr-code-detection/2',
@@ -13,10 +18,9 @@ with OakCamera() as oak:
     nn.config_nn(conf_threshold=.1)
     visualizer = oak.visualize(nn, fps=True)
 
-    visualizer.detections(bbox_style=BboxStyle.CORNERS,
-                          label_position=TextPosition.TOP_LEFT,
-                          fill_transparency=.5,
-                          thickness=2,
+    visualizer.detections(label_position=TextPosition.MID,
+                          fill_transparency=.4,
+                          thickness=6,
                           ).text(auto_scale=False, font_thickness=1, outline_color=(255, 255, 255), background_color=(0, 0, 255))
 
     oak.start(blocking=True)
