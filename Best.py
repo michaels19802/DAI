bdd2b6f7c6d8322909cc1cf1dda39ff9dd994ae2
Michael Stevens <michaels1980@gmail.com>
2023-08-14 12:31:17 +0100

    Testing

--- Best.py
+++ Best.py
@@ -1,21 +1,18 @@
-import cv2
-import time
+import cv2 as cv
 from roboflowoak import RoboflowOak
 
 
-rf = RoboflowOak(model='barcodes-6kvgt', confidence=0.1, overlap=0.5,
+rf = RoboflowOak(model='barcodes-6kvgt', confidence=0.05, overlap=0.5,
                  version='1', api_key='fAaLtouDRX3lJQdOgyJ1', rgb=True,
-                 depth=True, device=None, blocking=True)
+                 depth=False, device=None, blocking=True,
+                 advanced_config={'wide_fov': True, 'sensor_mode': 'THE_1080_P'})
 
 while True:
-    t0 = time.time()
     result, frame, raw_frame, depth = rf.detect()
     predictions = result['predictions']
-    t = time.time() - t0
-    print('INFERENCE', 1 / t)
-    print('PREDICTIONS', [p.json() for p in predictions])
 
-    cv2.imshow('frame', frame)
+    frame_small = cv.resize(frame, (frame.shape[1] // 2, frame.shape[0] // 2), interpolation=cv.INTER_AREA)
+    cv.imshow('frame_small', frame_small)
 
-    if cv2.waitKey(1) == ord('q'):
+    if cv.waitKey(1) == ord('q'):
         break
