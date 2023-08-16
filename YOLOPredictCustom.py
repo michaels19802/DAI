6db9a2eaddca2b8800b4d7ce269f9b3edfcac9b9
Michael Stevens <michael.stevens@hpponline.co.uk>
2023-08-16 11:25:12 +0100

    Testing

--- YOLOPredictCustom.py
+++ YOLOPredictCustom.py
@@ -7,12 +7,12 @@ model = YOLO('/home/michael/Dropbox/MichaelShared/DepthAI/yolov8n_custom/weights
 print(model.names)
 
 model.predict(
-    source='/home/michael/Desktop/img_small_screenshot_16.08.2023.png',
-    conf=0.2,
-    show=True,
-    save=False,
+    source='/home/michael/Desktop/Testing',
+    conf=0.4,
+    show=False,
+    save=True,
     save_crop=False,
-    classes=None
+    classes=[1, 2, 3]
 )
 
 cv.waitKey(0)
