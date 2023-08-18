7749a3755922d9a68ead906a27f1099d456b6970
Michael Stevens <michael.stevens@hpponline.co.uk>
2023-08-18 10:17:33 +0100

    Testing

--- YOLOPredictCustom.py
+++ YOLOPredictCustom.py
@@ -2,7 +2,7 @@ import cv2 as cv
 from ultralytics import YOLO
 
 
-model = YOLO('/home/michael/Dropbox/MichaelShared/DepthAI/yolov8n_custom/weights/best.pt')
+model = YOLO('17.pt')
 
 print(model.names)
 
@@ -12,7 +12,7 @@ model.predict(
     show=False,
     save=True,
     save_crop=False,
-    classes=[1, 2, 3]
+    classes=[0, 1, 2, 3]
 )
 
 cv.waitKey(0)
