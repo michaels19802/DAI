c6aedc6e0c360e67d5ccec0c880d90f7c43015e1
Michael Stevens <michaels1980@gmail.com>
2023-11-06 10:00:07 +0000

    Testing

--- YOLOTrain.py
+++ YOLOTrain.py
@@ -7,6 +7,6 @@ model = YOLO('yolov8n-seg.pt')
 model.train(
     data='/home/michael/Ultralytics/Datasets/data.yaml',
     imgsz=640,
-    epochs=300,
+    epochs=350,
     batch=16,
     name='yolov8n_custom')
