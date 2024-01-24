d374a91ace1291637eedadf3a3bbbc86f18aa639
Michael Stevens <michael.stevens@hpponline.co.uk>
2024-01-24 13:45:20 +0000

    Working

--- SimpleShowCam.py
+++ SimpleShowCam.py
@@ -1,7 +1,7 @@
 from depthai_sdk import OakCamera
 
 
-with OakCamera() as oak:
+with OakCamera(rotation=180) as oak:
     color = oak.create_camera('color', resolution='1080p')
     oak.visualize([color], fps=True)
     oak.start(blocking=True)
