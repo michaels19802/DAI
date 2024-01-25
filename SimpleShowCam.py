04037cb08a4963979b78adb0b35490135758a8ae
Michael Stevens <michael.stevens@hpponline.co.uk>
2024-01-25 13:24:41 +0000

    Working

--- SimpleShowCam.py
+++ SimpleShowCam.py
@@ -1,7 +1,11 @@
+import depthai as dai
 from depthai_sdk import OakCamera
 
 
 with OakCamera(rotation=180) as oak:
-    color = oak.create_camera('color', resolution='1080p')
+    color = oak.create_camera('color', resolution='1080p', fps=15)
+    color.config_color_camera(awb_mode=dai.CameraControl.AutoWhiteBalanceMode.TWILIGHT,
+                              effect_mode=dai.CameraControl.EffectMode.WHITEBOARD,
+                              sharpness=4)
     oak.visualize([color], fps=True)
     oak.start(blocking=True)
